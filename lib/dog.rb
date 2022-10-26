5e1265aa2920dc41ac32103ae72b98fbca16e2aa
wanjiku <annritawanjiku77@gmail.com>
2022-10-26 11:52:44 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
 
+
+    # setter 
+
+    def name= (name)
+        @name = name
+    end
+
+    # getter
+
+    def name
+        @name
+    end
+
+
+    # setter breed
+    def breed= (breed)
+        @breed = breed
+    end
+
+    # getter breed
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,30 @@
 class Person
 
+    # setter method
+    
+    def name= (name)
+        @name = name
+    end
+
+    # getter method
+    def name
+        @name
+    end
+
+
+    # setter job
+
+    def job= (job)
+        @job = job
+    end
+
+    # getter
+    def job
+        @job
+    end
 end
+
+kanye = Person.new
+kanye.name = ("kanye")
+
+puts kanye.name
